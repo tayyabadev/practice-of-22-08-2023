fc3066c743b0c91925c21f1647266a30c76340b0
Tayyaba Tabassum <tayyabadev@gmail.com>
2023-08-23 20:45:24 +0500

    Today I practice loops and patterns

---
+++ guessing-number.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+int x=12;
+cout<<"Guess the number :"<<endl;
+cin>>x;
+    while(12!=x){
+        cout<<"You have guessed the wrong number ."<<endl;
+        cout<<"Enter another number:"<<endl;
+        cin>>x;}
+        cout<<"hurray!You have guessed the number.";
+return 0;}
